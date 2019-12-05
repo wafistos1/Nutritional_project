5f1e244efa99ce2b9e35374701e53e4b36afc249
wafistos1 <mameri.wafi@gmail.com>
2019-12-05 23:54:41 +0100

    update setting

--- app8/settings.py
+++ app8/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +156,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
