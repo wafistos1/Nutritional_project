a2b6f294c9cb8165e966b4564e97f67aae129775
wafistos1 <mameri.wafi@gmail.com>
2019-12-05 23:44:22 +0100

    update setting

--- app8/settings.py
+++ app8/settings.py
@@ -144,9 +144,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
