f61f165fead51eb0d16166e2a3cf087cdef9624d
Wafi <mameri.wafi@gmail.com>
2020-03-04 19:14:28 +0100

    add new model rating

--- store/models.py
+++ store/models.py
@@ -15,13 +15,7 @@ class Categorie(models.Model):
 class Product(models.Model):
     """ Class
     """
-    RATE_CHOICES = (
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4),
-        (5, 5),
-    )
+
     name = models.CharField(_('Nom produit'), max_length=500, unique=True)
     grade = models.CharField(max_length=40)
     images = models.URLField(max_length=500)
@@ -29,7 +23,6 @@ class Product(models.Model):
     detail_igredient = models.TextField(default='None')
     url = models.URLField(max_length=500, default='None')
     detail_nutrition_url = models.URLField(max_length=500, default='None')
-    rate_product = models.IntegerField(max_length=2, choices=RATE_CHOICES, null=True)
     
 
     def __str__(self):
@@ -50,3 +43,17 @@ class Favorite(models.Model):
 
     def __str__(self):
         return f"produit: {self.product_choice.name} , product_substitute:{self.product_favorite.name}"
+
+
+class rating(models.Model):
+    RATE_CHOICES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    )
+    rating = models.IntegerField(max_length=2, choices=RATE_CHOICES, null=True)
+    product_rating = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_rating = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_voting = models.BooleanField(default=False)
